c62f36da363cdfa5e87c7bfde56101521b155ad1
Avery Kyle Arenas <kyruken@gmail.com>
2022-09-18 14:35:44 -0700

    complete design of start quiz page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import './App.css'
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="start-page">
+      <div className="start-quiz">
+        <h1>Quizzical</h1>
+        <p>Answer these 5 questions!</p>
+        <button>Start quiz</button>
+      </div>
 
     </div>
   )
