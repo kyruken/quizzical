cc2c9bf66e20b62587e7345f3ee67bc4effc657c
Avery Kyle Arenas <kyruken@gmail.com>
2022-09-18 15:41:19 -0700

    add fetch get request

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
 export default function Quiz() {
+    fetch('https://opentdb.com/api.php?amount=5')
+    .then((response) => response.json())
+    .then((data) => console.log(data));
     return (
         <div>
             Heres my quiz
